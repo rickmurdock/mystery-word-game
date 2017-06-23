8ae216780b2a7067eb9f0555b717c498efc680c6
rickmurdock <rick.murdock@live.com>
2017-06-23 11:53:28 -0500

    Changes to the display for basic testing

--- server.js
+++ server.js
@@ -26,12 +26,17 @@ app.use(function (req, res, next) {
     let randomNumber = Math.floor((Math.random() * words.length-1) + 1)
     game.word = words[randomNumber];
     game.guessesLeft = 8;
-    game.lettersGuessed = [];
+    game.lettersGuessedWrong = [];
+    game.lettersGuessedRight = [];
+    game.btnText = 'Make a guess';
+    game.status = '';
+    game.message = '';
+    // game.display = '';
+    game.display = buildDisplay(game);
   }
-  console.log("session IS", req.session);
+  // console.log("session IS", req.session);
   next();
 });
-
 
 //ROUTES
 app.get('/', function(req, res) {
@@ -40,15 +45,25 @@ app.get('/', function(req, res) {
 
 app.post('/', function(req, res) {
   var game = req.session.game;
+  var allLettersGuessed = game.lettersGuessedWrong.concat(game.lettersGuessedRight);
+  console.log('allLettersGuessed ',allLettersGuessed);
+  // check if letter already guessed else if not found or found
+  if (allLettersGuessed.indexOf(req.body.guessLetter) > -1) {
+    game.message = 'You already guessed letter ' + req.body.guessLetter;
+  } else { 
 
-  console.log('game.word', game.word);
-  var n = game.word.indexOf(req.body.guessLetter);
-  console.log("======N=====", n);
-  if (n == -1) {
-    game.guessesLeft -= 1;
-    
+    var n = game.word.indexOf(req.body.guessLetter);
+    if (n == -1) {
+      // if (req.body.guessLetter.indexOf(game.letter)) 
+      game.message = 'Bad guess...try again!';
+      game.guessesLeft -= 1;
+      game.lettersGuessedWrong.push(req.body.guessLetter);
+    } else {
+      game.lettersGuessedRight.push(req.body.guessLetter);
+      game.message = 'You guessed correctly.';
+    }
   }
-
+console.log("session IS", req.session);
   res.render('index', { game: req.session.game });
 });
 
@@ -56,3 +71,14 @@ app.post('/', function(req, res) {
 app.listen(port, function() {
   console.log('The game server is up and running on port', port);
 });
+
+function buildDisplay(game) {
+  var showText = '';
+  // game.word.forEach(function(letter) {
+  //   showText += showText + letter + ' ';
+  // });
+  for (let i = 0; i < game.word.length; i++) {
+    showText = showText + game.word[i] + ' ';
+  }
+  return showText;
+};
