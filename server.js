6c8a3f26d02ff87e31f2950e295284d0c84b0fed
rickmurdock <rick.murdock@live.com>
2017-06-25 18:08:54 -0500

    Major changes for win lose.

--- server.js
+++ server.js
@@ -25,63 +25,82 @@ app.use(function (req, res, next) {
   var game = req.session.game;
   if (!game) {
     game = req.session.game = {};
-    game.mode = 'easy';
-    game.word = findRandomWord(game.mode);
+    game.mode = '';
     game.guessesLeft = 8;
     game.lettersGuessed = [];
-    game.btnText = 'Make a guess';
+    game.btnText = 'Play game';
     game.status = '';
+    game.lose = false;
+    game.playing = false;
     game.message = '';
     game.display = '';
-    // game.display = buildDisplay(game);
   }
-  game.display = buildDisplay(game);
   next();
 });
 
 //ROUTES
 app.get('/', function(req, res) {
-  req.session.game.display = buildDisplay(req.session.game);
+  if (req.session.game.playing || req.session.game.btnText != 'Play game') {
+    req.session.game.display = buildDisplay(req.session.game);
+  }
+  // req.session.game.display = buildDisplay(req.session.game);
   res.render('index', { game: req.session.game });
 });
 
 app.post('/', function(req, res) {
   var game = req.session.game;
-  req.checkBody("guessLetter", "You must enter a letter!").notEmpty();
-  var errors = req.validationErrors();
-  console.log('ERRORS =', errors);
+  if (game.playing) {
+    req.checkBody("guessLetter", "You must enter a letter!").notEmpty().isAlpha();
+    var errors = req.validationErrors();
+    console.log('ERRORS =', errors);
     if (errors) {
-    game.message = errors[0].msg;
-  } else {
-
-  // var game = req.session.game;
-  console.log('lettersGuessed ',game.lettersGuessed);
-  // check if letter already guessed else if not found or found
-  // if (allLettersGuessed.indexOf(req.body.guessLetter) > -1) {
-  if (game.lettersGuessed.indexOf(req.body.guessLetter) > -1) {
-    game.message = 'You already guessed letter ' + req.body.guessLetter;
-  } else { 
-
-    var n = game.word.indexOf(req.body.guessLetter);
-    if (n == -1) {
-      // if (req.body.guessLetter.indexOf(game.letter)) 
-      game.message = 'Bad guess...try again!';
-      game.guessesLeft -= 1;
-      game.lettersGuessed.push(req.body.guessLetter);
-      if (game.guessesLeft == 0) {
-        game.message = 'YOU ARE A LOSER!!!!';
-        game.status = 'lose';
-      }
+      game.message = errors[0].msg;
     } else {
-      game.lettersGuessed.push(req.body.guessLetter);
-      game.message = 'You guessed correctly.';
+      console.log('lettersGuessed ',game.lettersGuessed);
+      // check if letter already guessed else if not found or found
+        // if (allLettersGuessed.indexOf(req.body.guessLetter) > -1) {
+      if (game.lettersGuessed.indexOf(req.body.guessLetter.toUpperCase()) > -1) {
+        game.message = 'You already guessed letter ' + req.body.guessLetter.toUpperCase();;
+      } else { 
+        var n = game.word.indexOf(req.body.guessLetter.toUpperCase());
+        if (n == -1) {  // Bad Guess
+          game.message = 'Bad guess...try again!';
+          game.guessesLeft -= 1;
+          game.lettersGuessed.push(req.body.guessLetter.toUpperCase());
+          if (game.guessesLeft == 0) {
+            game.message = '';
+            game.btnText = 'Try again';
+            game.status = 'You lose!';
+            game.playing = false;
+            game.lose = true;
+          }
+        } else {  // Correct Guess
+          game.lettersGuessed.push(req.body.guessLetter.toUpperCase());
+          game.message = '';
+          // check for win ---------------------------
+          req.session.game.display = buildDisplay(req.session.game);
+          if (game.display.indexOf(' ') ==  -1) {
+            game.message = '';
+            game.btnText = 'Try again';
+            game.status = 'You win!';
+            game.playing = false;
+            game.lose = false;
+          }
+        }
+      }
     }
+  } else {
+    game.playing = true;
+    game.btnText = "Make a guess";
+    game.mode = req.body.mode;
+    game.word = findRandomWord(game.mode);
+
+    game.lose = false;
+    game.guessesLeft = 8;
+    game.lettersGuessed = [];
   }
 
-}
-
-
-console.log("session IS", req.session);
+  console.log("session IS", req.session);
   // res.render('index', { game: req.session.game });  // redirect ???
   res.redirect('/');
 });
@@ -97,7 +116,7 @@ function buildDisplay(game) {
     if (game.lettersGuessed.indexOf(game.word[i]) > -1) {
        showText.push(game.word[i].toUpperCase());
      } else {
-       if (game.status == 'lose') {
+       if (game.lose == true) {
           showText.push(game.word[i].toUpperCase());
        } else {
           showText.push(' ');
@@ -111,8 +130,6 @@ function findRandomWord(mode) {
   let randomWord;
   let wordLength = 0;
   let wordFound = false;
-  // let randomNumber = Math.floor((Math.random() * words.length-1) + 1)
-  // randomWord = words[randomNumber];
 
   while (!wordFound) {
     let randomNumber = Math.floor((Math.random() * words.length-1) + 1)
@@ -134,5 +151,5 @@ function findRandomWord(mode) {
     }
   }
 
-  return randomWord;
+  return randomWord.toUpperCase();
 };
