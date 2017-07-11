943b9754bce9a74d66ba0ab85f701544d3853e4b
rickmurdock <rick.murdock@live.com>
2017-07-11 00:18:40 -0500

    Cleaned up for project submission.

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ const app = express();
 const port = process.env.port || 3000;
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 
-// var snd = new Audio("jeopardyThemeSong.mp3");
-
-// snd.play;
-
 // SET VIEW ENGINE
 app.engine("mustache", mustacheExpress());
 app.set("views", "./public");
@@ -47,7 +43,6 @@ app.get('/', function(req, res) {
   if (req.session.game.playing || req.session.game.btnText != 'Play game') {
     req.session.game.display = buildDisplay(req.session.game);
   }
-  // req.session.game.display = buildDisplay(req.session.game);
   res.render('index', { game: req.session.game });
 });
 
@@ -62,7 +57,6 @@ app.post('/', function(req, res) {
     } else {
       console.log('lettersGuessed ',game.lettersGuessed);
       // check if letter already guessed else if not found or found
-        // if (allLettersGuessed.indexOf(req.body.guessLetter) > -1) {
       if (game.lettersGuessed.indexOf(req.body.guessLetter.toUpperCase()) > -1) {
         game.message = 'You already guessed letter ' + req.body.guessLetter.toUpperCase();;
       } else { 
@@ -81,18 +75,14 @@ app.post('/', function(req, res) {
         } else {  // Correct Guess
           game.lettersGuessed.push(req.body.guessLetter.toUpperCase());
           game.message = '';
-          // check for win ---------------------------
-          req.session.game.display = buildDisplay(req.session.game); ///////
-          // if (game.display.indexOf(' ') ==  -1) {
+          // check for win
+          req.session.game.display = buildDisplay(req.session.game);
           if (game.display.indexOf('<div class="letters" style="color:red"> </div>') ==  -1) {
-            
             game.message = '';
             game.btnText = 'Try again';
             game.status = 'You win!';
             game.playing = false;
             game.lose = true;
-            console.log("WIIIIIIINNNNNNNNN");
-            console.log('game PLAYING', game.playing);
           }
         }
       }
@@ -100,17 +90,14 @@ app.post('/', function(req, res) {
   } else { // !game.playing
     game.playing = true;
     game.btnText = "Make a guess";
-    game.status = '';                   /////
+    game.status = '';
     game.mode = req.body.mode;
     game.word = findRandomWord(game.mode);
-
     game.lose = false;
     game.guessesLeft = 8;
     game.lettersGuessed = [];
   }
-
   console.log("session IS", req.session);
-  // res.render('index', { game: req.session.game });  // redirect ???
   res.redirect('/');
 });
 
@@ -124,30 +111,17 @@ function buildDisplay(game) {
   var value;
   for (let i = 0; i < game.word.length; i++) {
     if (game.lettersGuessed.indexOf(game.word[i]) > -1) {
-      //  showText.push(game.word[i].toUpperCase());
-       
-  console.log('11111111');    
-      //  showText.push('<span style="color:blue">' + game.word[i].toUpperCase() + '</span>');
-
-          value = '<div class="letters" style="color:blue">' + game.word[i].toUpperCase() + '</div>'
-          showText.push(value);
-
-     } else {
-       if (game.lose == true) {
-      //  if (game.playing == false) {
-          // showText.push(game.word[i].toUpperCase());
-console.log('22222222');    
-          value = '<div class="letters" style="color:red">' + game.word[i].toUpperCase() + '</div>'
-          showText.push(value);
-
-       } else {
-          // showText.push(' ');
-console.log('3333333333');    
-          var value = '<div class="letters" style="color:red"> </div>'
-          showText.push(value);
-
-       }
-     }
+      value = '<div class="letters" style="color:blue">' + game.word[i].toUpperCase() + '</div>'
+      showText.push(value);
+    } else {
+      if (game.lose == true) {
+        value = '<div class="letters" style="color:red">' + game.word[i].toUpperCase() + '</div>'
+        showText.push(value);
+      } else {   
+        var value = '<div class="letters" style="color:red"> </div>'
+        showText.push(value);
+      }
+    }
   }
   return showText;
 };
